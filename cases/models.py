5d58f1fbdc4212d51b3ff72c2456da143b863afa
abdulazeeznaji <abdulazeeznaji@yahoo.com>
2017-05-11 15:42:48 +0300

    define bluePrint schemas

--- cases/models.py
+++ cases/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Cases(models.Model):
+    description = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField('date published')
+    
--- cases/views.py
+++ cases/views.py
@@ -8,4 +8,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls inde.")
+    return HttpResponse("Hello, world. You're at the polls index.")
